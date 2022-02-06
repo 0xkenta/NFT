889d522d1eb486483fa0b87260b27179f3b1ca25
0xkenta <kenta0558kiichi614@gmail.com>
2022-02-06 16:11:11 +0900

    add new tests

--- onchain_NFT2_with_SVG/test/nft.ts
+++ onchain_NFT2_with_SVG/test/nft.ts
@@ -4,12 +4,14 @@ import { solidity } from 'ethereum-waffle';
 import { NFT } from '../typechain-types'
 import { Signer, constants } from "ethers";
 
-use(solidity);
+use(solidity)
 
 describe("NFT.sol", () => {
     let deployer: Signer
     let user1: Signer
+    let user2: Signer
     let user1Address: string
+    let user2Address: string
     let nftContract: NFT
 
     const message = "HELLO WORLD"
@@ -18,8 +20,9 @@ describe("NFT.sol", () => {
     const emptyAddress = constants.AddressZero
 
     beforeEach(async () => {
-        [deployer, user1] = await ethers.getSigners()
+        [deployer, user1, user2] = await ethers.getSigners()
         user1Address = await user1.getAddress()
+        user2Address = await user2.getAddress()
         const nftFactory = await ethers.getContractFactory("NFT")
         nftContract = await nftFactory.deploy() as NFT
     })
@@ -55,10 +58,50 @@ describe("NFT.sol", () => {
                 .to.emit(nftContract, "Transfer")
                 .withArgs(emptyAddress, user1Address, 1)
         })
-        // it("update total supply", async () => {
-        //     await nftContract.mint(message, color)
-        //     await nftContract.connect(user1).mint(message, color)
-        //     expect(await nftContract.totalSupply()).to.be.equal(2)
-        // })
+        it("update total supply", async () => {
+            const randomNumber = Math.floor(Math.random() * 10)
+            for (let i = 0; i < randomNumber; i++) {
+                await nftContract.connect(user1).mint(message, color)
+            }
+            expect(await nftContract.totalSupply()).to.be.equal(randomNumber)
+        })
+        it("update tokenOfOwnerByIndex and tokenByIndex", async () => {
+            await nftContract.connect(user1).mint(message, color)
+            await nftContract.connect(user2).mint(message, color)
+            await nftContract.connect(user1).mint(message, color)
+            await nftContract.connect(deployer).mint(message, color)
+            await nftContract.connect(user2).mint(message, color)
+            
+            expect(await nftContract.tokenOfOwnerByIndex(user1Address, 0)).to.be.equal(1)
+            expect(await nftContract.tokenOfOwnerByIndex(user1Address, 1)).to.be.equal(3)
+            expect(await nftContract.tokenOfOwnerByIndex(user2Address, 0)).to.be.equal(2)
+            expect(await nftContract.tokenOfOwnerByIndex(user2Address, 1)).to.be.equal(5)
+
+            expect(await nftContract.tokenByIndex(0)).to.be.equal(1)
+            expect(await nftContract.tokenByIndex(4)).to.be.equal(5)
+        })
+    })
+
+    describe("tokenURI()", () => {
+        it("revert if there is no minted token id", async () => {
+            await expect(nftContract.tokenURI(2)).to.be.revertedWith("NO_NFT_WITH_THIS_ID")
+        })
+    })
+
+    describe("buildMetadata()", () => {
+        it("return metadata", async () => {
+            await nftContract.mint(message, color)
+            const tokenURI = await nftContract.tokenURI(1)
+            const { name, description, image } = JSON.parse(
+                Buffer.from(tokenURI.replace('data:application/json;base64,', ''), 'base64').toString(
+                    'ascii',
+                ),
+            )
+
+            expect(name).to.be.equal("NFT 1")
+            expect(description).to.be.equal("Test NFT only background and message in the center of image")
+            expect(image).to.not.be.undefined
+            // image base64 todo
+        })
     })
 })
