3e7f8f0aaab3132b856a0aac9647b8ab46632524
0xkenta <kenta0558kiichi614@gmail.com>
2022-02-07 21:40:15 +0900

    update config to deploy

--- onchain_NFT2_with_SVG/hardhat.config.ts
+++ onchain_NFT2_with_SVG/hardhat.config.ts
@@ -4,7 +4,22 @@
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-ethers"
 import '@typechain/hardhat'
+import 'dotenv/config'
+import "@nomiclabs/hardhat-etherscan"
+
+const { API_URL, PRIVATE_KEY, POLYGONSCAN_API } = process.env
 
 module.exports = {
   solidity: "0.8.9",
-};
+  networks: {
+    polygon_mumbai: {
+       url: API_URL,
+       accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_API  
+    }
+  }
+}
