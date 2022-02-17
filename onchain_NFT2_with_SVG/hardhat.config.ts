a09fbda32ada6a6b55cdae723d59bfd9894349a2
0xkenta <kenta0558kiichi614@gmail.com>
2022-02-17 10:40:05 +0900

    add ropsten

--- onchain_NFT2_with_SVG/hardhat.config.ts
+++ onchain_NFT2_with_SVG/hardhat.config.ts
@@ -7,7 +7,7 @@ import '@typechain/hardhat'
 import 'dotenv/config'
 import "@nomiclabs/hardhat-etherscan"
 
-const { API_URL, PRIVATE_KEY, POLYGONSCAN_API } = process.env
+const { API_URL, API_ROPSTEN_URL, PRIVATE_KEY, POLYGONSCAN_API, ETHERSCAN_API } = process.env
 
 module.exports = {
   solidity: "0.8.9",
@@ -15,10 +15,15 @@ module.exports = {
     polygon_mumbai: {
        url: API_URL,
        accounts: [`0x${PRIVATE_KEY}`]
+    },
+    ropsten: {
+      url: API_ROPSTEN_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
   etherscan: {
     apiKey: {
+      ropsten: ETHERSCAN_API,
       polygonMumbai: POLYGONSCAN_API  
     }
   }
